9e595b5ce1f8d68ca184efe5b12fbf9a9c331d9c
NIKHIL DWIVEDI <nndwivedi9996@gmail.com>
2023-07-16 05:00:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ import sys
 # Install dependencies
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
+# Give the "prohunt" script executable permissions
+    script_executable = "/usr/local/bin/prohunt.py"  # Update the path if needed
+    os.chmod(script_executable, +x)
+
 # Get the path to the "prohunt.py" script
 script_path = os.path.join(os.getcwd(), "prohunt.py")
 
@@ -21,7 +25,11 @@ if script_path not in path_env:
     os.environ["PATH"] = new_path
     
     # Move the script file to a directory in the PATH
-    shutil.move(script_path, "/usr/local/bin/prohunt")  # Update the destination directory if needed
+    shutil.copy(script_path, "/usr/local/bin/prohunt")  # Update the destination directory if needed
+    
+    # Give the "prohunt" script executable permissions
+    script_executable_path = "/usr/local/bin/prohunt.py"  # Update the path if needed
+    os.chmod(script_executable_path, 0o755)
 
 # Provide setup completion message
 print("Prohunt setup completed successfully.")
